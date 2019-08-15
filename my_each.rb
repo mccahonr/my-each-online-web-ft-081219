8d28d350077d09a4d9d0abe3c48c7ee478ac12a0
Ryan McCahon <mccahonr@yahoo.com>
2019-08-15 23:59:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,5 @@
-def my_each(array, number) # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  collection = []# code here
   i = 0
   while i < array.length do |number|
     yield(array[i])
